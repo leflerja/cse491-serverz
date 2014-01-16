5a22fcc9b0b6687fecfe0045cb42c25a4e34485c
koppmana <koppmana@gmail.com>
2014-01-16 01:37:08 -0500

    Did things

--- server.py
+++ server.py
@@ -4,29 +4,59 @@ import socket
 import time
 import webob
 
-s = socket.socket()         # Create a socket object
-host = socket.getfqdn() # Get local machine name
-port = random.randint(8000, 9999)
-s.bind((host, port))        # Bind to the port
 
-print 'Starting server on', host, port
-print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
-s.listen(5)                 # Now wait for client connection.
+class Cse491server(object):
 
-def main():
-    print 'Entering infinite loop; hit CTRL-C to exit'
-    while True:
+    #initiliaze server then listen for client
+    def __init__(self):
+        self.sock = socket.socket()         # Create a socket object
+        self.host = socket.getfqdn() # Get local machine name
+        self.port = random.randint(8000, 9999)
+        sock.bind((host, port))        # Bind to the port
+
+        print 'Starting server on', host, port
+        print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+
+        self.sock.listen(5)                 # Now wait for client connection.
+
+    def wait_for_connect(self):
         # Establish connection with client.    
-        c, (client_host, client_port) = s.accept()
-        response = init_response()
+        self.conn, (client_host, client_port) = sock.accept()
         
         print 'Got connection from', client_host, client_port
-        c.send(response)
-        c.send('Thank you for connecting')
-        c.send("good bye.")
-        c.close()
 
+    #send response to client
+    def send_responses(self):
+            cont_type = "Content-Type: text/html"
+            response_type = "HTTP/1.1"
+            response_status = "200"
+            response_status_text = "OK"
+            self.response_body = "<html><body><h1>Hello world!</h1></body></html>"
+            
+            self.conn.send("%s %s %s" % response_type, response_status, response)
+            self.conn.send("\n")
+            self.conn.send(response_body)
+
+    def close_conn():
+        self.conn.send('Thank you for connecting')
+        self.conn.send("good bye.")
+        self.conn.close()
+
+        
+def main():
+    server = Cse491sever()
+
+    server.wait_for_connect()
+
+    #print 'Entering infinite loop; hit CTRL-C to exit'
+    
+    while True:
+        server.send_responses()
+    
+    server.close_conn()
+    
+    
 def init_response():
     res = webob.Response()
     res.status_int = 200

5bc00c29130763980dfb534614814095a50e73b4
Allen Koppman <koppmana@gmail.com>
2014-01-15 23:29:56 -0500

    .

--- server.py
+++ server.py
@@ -14,11 +14,25 @@ print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
 s.listen(5)                 # Now wait for client connection.
 
-print 'Entering infinite loop; hit CTRL-C to exit'
-while True:
-    # Establish connection with client.    
-    c, (client_host, client_port) = s.accept()
-    print 'Got connection from', client_host, client_port
-    c.send('Thank you for connecting')
-    c.send("good bye.")
-    c.close()
+def main():
+    print 'Entering infinite loop; hit CTRL-C to exit'
+    while True:
+        # Establish connection with client.    
+        c, (client_host, client_port) = s.accept()
+        response = init_response()
+        
+        print 'Got connection from', client_host, client_port
+        c.send(response)
+        c.send('Thank you for connecting')
+        c.send("good bye.")
+        c.close()
+
+def init_response():
+    res = webob.Response()
+    res.status_int = 200
+    res.headerlist = [('Content-Type', 'text/html')]
+    res.body = "<h1>Hello World!</h1>"
+
+    return res
+
+main()
